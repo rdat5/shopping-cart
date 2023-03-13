8133bd8f992c4c4cf3f2129d8d619757cd28a75e
rdat5 <42378063+rdat5@users.noreply.github.com>
2023-03-13 01:11:05 -0700

    Add feature to populate shop from inventory data

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,17 +1,11 @@
 import Card from "./Card";
+import { cardData } from "../data";
 
 export default function Shop() {
     return (
         <div className="shop-page">
             <div className="item-container">
-                <Card itemIndex={0}/>
-                <Card itemIndex={1}/>
-                <Card itemIndex={2}/>
-                <Card itemIndex={3}/>
-                <Card itemIndex={4}/>
-                <Card itemIndex={5}/>
-                <Card itemIndex={6}/>
-                <Card itemIndex={7}/>
+                {cardData.map(item => <Card key={item.key}itemIndex={item.key}/>)}
             </div>
         </div>
     );
